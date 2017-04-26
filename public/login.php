1d5f6f0e230fc227b49a5976f88ea3a7e5db333a
Rogerio Tadim <rogeriotadim@gmail.com>
2017-04-26 11:54:44 -0300

    inclusao do login

---
+++ public/login.php
@@ -0,0 +1,39 @@
+<?php
+require 'vendor/autoload.php';
+$app = new Silex\Application();
+$app['debug'] = true;
+
+include 'bootstrap.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Appch\Middleware\Logging as AppchLogging;
+use Appch\Models\User;
+
+$app ->get('/login/v1/login', function(Request $request) {
+    $userRequest = $request->headers->get("php-auth-user");
+    $passRequest = $request->headers->get("php-auth-pw");
+
+    $user = new User();
+    $userLoged = $user->login($userRequest, $passRequest);
+    if(!$userLoged){
+        return false;
+    }
+
+    $payload = [];
+    $payload[$userLoged->id] =
+    [
+        'username' => $userLoged->username,
+        'id' => $userLoged->id,
+        'email' => $userLoged->email
+    ];
+
+    return json_encode($payload, JSON_UNESCAPED_SLASHES);
+
+});
+
+
+$app->run();
+
+
+?>
--- public/src/Appch/Models/User.php
+++ public/src/Appch/Models/User.php
@@ -2,8 +2,20 @@
 
 namespace Appch\Models;
 
-class User extends \Illuminate\Database\Eloquent\Model
-{
+class User extends \Illuminate\Database\Eloquent\Model {
+
+    public function login($user, $pass){
+        $userLoged = User::where('email', '=', $user)->take(1)->get();
+
+        $userPassword=$userLoged[0]->getAttributes()['password'];
+        if(!password_verify($pass, $userPassword)){
+            return false;
+        }
+
+        return $userLoged[0];
+
+    }
+
     public function authenticate($apikey)
     {
         $user = User::where('apikey', '=', $apikey)->take(1)->get();
